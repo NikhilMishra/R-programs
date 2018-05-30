885db412457d702d3fd7a8716caa9b46821890d0
NikhilMishra <mishranikh@gmail.com>
2018-05-30 11:57:43 -0700

    Added comments and some changes to graph
    excluded calibration data and fixed magnitude label

--- first R.R
+++ first R.R
@@ -1,2 +1,6 @@
-plot <- ggplot(ax[sample(nrow(ax), 1000), ], aes(x=Time, y=X, size = mag )) + geom_point()
-plot + labs(y="X Acceleration (m/s^2)", x = "Time (s)", size = "magnitude (m/s^s)")
+#create subset of acceleration data (ax) that excludes calibration data
+newData <- subset(ax, Time >= 110, select=c(X, mag, Time))
+#use a random sample of 1500 data points to create graph of Time vs X acceleration and magnitude
+plot <- ggplot(newData[sample(nrow(newData), 1500), ], aes(x=Time, y=X, color = mag )) + geom_point()
+#label axis and title
+plot + labs(y="X Acceleration (m/s^2)", x = "Time (s)", color = "magnitude (m/s^2)", title = "Time vs X Acceleration and Magnitude of Acceleration")
