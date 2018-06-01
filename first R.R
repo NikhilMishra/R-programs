184af19e93693c28c391ef56740c49232517fadd
NikhilMishra <mishranikh@gmail.com>
2018-06-01 11:17:32 -0700

    Centered title

--- first R.R
+++ first R.R
@@ -3,4 +3,6 @@ newData <- subset(ax, Time >= 110, select=c(X, mag, Time))
 #use a random sample of 1500 data points to create graph of Time vs X acceleration and magnitude
 plot <- ggplot(newData[sample(nrow(newData), 1500), ], aes(x=Time, y=X, color = mag )) + geom_point()
 #label axis and title
-plot + labs(y="X Acceleration (m/s^2)", x = "Time (s)", color = "magnitude (m/s^2)", title = "Time vs X Acceleration and Magnitude of Acceleration")
+theme_update(plot.title = element_text(hjust = 0.5))
+plot + labs(y="X Acceleration (m/s^2)", x = "Time (s)", color = "magnitude (m/s^2)") +
+ggtitle("Time vs X Acceleration and Magnitude of Acceleration")
