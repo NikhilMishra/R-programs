c5f58d7d2e27b908713e253cec9ff09708176f39
NikhilMishra <mishranikh@gmail.com>
2018-06-01 14:10:31 -0700

    print mean and median and added more comments

--- first R.R
+++ first R.R
@@ -2,7 +2,13 @@
 newData <- subset(ax, Time >= 110, select=c(X, mag, Time))
 #use a random sample of 1500 data points to create graph of Time vs X acceleration and magnitude
 plot <- ggplot(newData[sample(nrow(newData), 1500), ], aes(x=Time, y=X, color = mag )) + geom_point()
-#label axis and title
+#center title
 theme_update(plot.title = element_text(hjust = 0.5))
+#label axis and title
 plot + labs(y="X Acceleration (m/s^2)", x = "Time (s)", color = "magnitude (m/s^2)") +
 ggtitle("Time vs X Acceleration and Magnitude of Acceleration")
+#print mean and median of data
+meanval <- mean(newData$X, na.rm=TRUE)
+medianval <- median(newData$X, na.rm=TRUE)
+cat("The mean of the data is: ", meanval)
+cat("the median of the data is: ", medianval)
